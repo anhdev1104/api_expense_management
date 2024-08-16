30f0847e309d596e9f200b97f6112c77dee364d2
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-08-17 01:39:37 +0700

    handle spendlimit api

--- src/controllers/spendlimit.js
+++ src/controllers/spendlimit.js
@@ -33,6 +33,15 @@ export const addSpendlimit = async (req, res) => {
   }
 };
 
+export const updateSpendlimit = async (req, res) => {
+  try {
+    const spendlimitUpdate = await Spendlimit.findByIdAndUpdate(req.params.id, req.body);
+    return res.status(200).json(spendlimitUpdate);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 export const deleteSpendlimit = async (req, res) => {
   try {
     await Spendlimit.deleteMany({ category: req.params.category });
--- src/routes/spendlimit.js
+++ src/routes/spendlimit.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { getSpenlimit, addSpendlimit, getSpenlimitDetails, deleteSpendlimit } from '../controllers/spendlimit.js';
+import {
+  getSpenlimit,
+  addSpendlimit,
+  getSpenlimitDetails,
+  deleteSpendlimit,
+  updateSpendlimit,
+} from '../controllers/spendlimit.js';
 const router = express.Router();
 
 router.get('/spendlimit', getSpenlimit);
 router.get('/spendlimit/:category', getSpenlimitDetails);
 router.post('/spendlimit', addSpendlimit);
+router.put('/spendlimit/:id', updateSpendlimit);
 router.delete('/spendlimit/:category', deleteSpendlimit);
 
 export default router;
